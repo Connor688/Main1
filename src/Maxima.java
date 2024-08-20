e47f502c958d3a6b08f1a13c4fc4329390879dd6
Connor688 <ser.ermilov.89@mail.ru>
2024-08-20 23:44:54 +0300

    тест

---
+++ src/Maxima.java
@@ -0,0 +1,5 @@
+public class Maxima {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/lesson_2/main.java
@@ -0,0 +1,45 @@
+package lesson_2;
+
+public class main {
+    public static void main(String[] args) {
+        // Задача 1
+        int a4 = 5;
+        int b4 = 2;
+        double c4 = (double) a4 / b4;
+        System.out.println(c4);
+        // В каждой кассе лежит по 2.5 рублей
+
+
+        // Задача 2
+        byte a3 = 50;
+        byte b3 = 20;
+        int c = a3 * b3;
+        System.out.println(c);
+        // У квадрата все стороны равны. В нашем случае это прямоугольник с площадью 1000
+
+
+        // Задача 3
+        int a1 = 9;
+        int b1 = 3;
+        int c1 = a1 / b1;
+        System.out.println(c1);
+        int a2 = 15;
+        int b2 = 3;
+        int c2 = a2 / b2;
+        System.out.println(c2);
+        int y = c2-c1;
+        System.out.println(y);
+        // На 2 минуты быстрее Вася съедает банан когда голоден
+
+
+        // Задача 4
+      int a = 3;
+      int b = 6;
+      int sum = 3 + a + b;
+        System.out.println("sum = " + sum);
+
+
+    }
+
+
+}
